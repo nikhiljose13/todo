088b25dff715962b6502c682fc125115f0846dda
nikhil <nikhiljose59@gmail.com>
2024-01-12 14:08:39 +0530

    viewset

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from api.serializers import Userserializers,Todoserializers
-
+from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
@@ -44,3 +44,23 @@ class   TodosView(ViewSet):
         qs=Todos.objects.get(id=id)
         serializer=Todoserializers(qs)
         return Response(data=serializer.data)
+     
+    def destroy(self,requset,*args,**kwargs):
+        id=kwargs.get("pk")
+        qs=Todos.objects.get(id=id)
+        if qs.user==requset.user:
+             qs.delete()
+        else:
+             raise serializers.ValidationError("poda mone")
+
+        return Response(data={"message":"deleted"})
+    
+    def update(self,request,*args,**kwargs):
+            id=kwargs.get("pk")
+            todo_object=Todos.objects.get(id=id)
+            Serializers=Todoserializers(data=request.data,instance=todo_object)
+            if Serializers.is_valid():
+                  Serializers.save()
+                  return Response(data=Serializers.data)
+            else:
+                  return  Response(data=Serializers.errors)
